1592c07f61464ccce511fc23c9ffdfb4a223560d
cavalown <cavalown@gmail.com>
2022-09-20 02:24:51 +0800

    Feat: test function - hello & cron job

---
+++ README.md
@@ -0,0 +1,13 @@
+# Taiwan Earthquack Check
+
+## Firebase Function
+### Init project with firebase
+```bash
+$ firebase init
+```
+
+### Deploy only function
+[官方部署參考文件](https://firebase.google.com/docs/functions/manage-functions)
+```bash
+$ firebase deploy --only functions
+```
--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,15 @@ const functions = require("firebase-functions");
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+
+// Test: first function
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
+});
+
+// Test: cron job function
+exports.scheduledFunction = functions.pubsub.schedule('every 1 minutes').onRun((context) => {
+  console.log('This will be run every 1 minutes!');
+  return null;
 });
